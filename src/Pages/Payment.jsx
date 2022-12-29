afef5f62eb29d5767058f40c42eabe1d553c05d6
Rayhan Ahmed <dev.rayhan7@gmail.com>
2022-12-29 11:48:46 +0600

    assets added

---
+++ src/Pages/Payment.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaInfoCircle } from 'react-icons/fa';
+import { BsHandIndex } from 'react-icons/bs';
+const Payment = () => {
+
+    return (
+        <div className='payment'>
+            {/* <FaInfoCircle/>
+            <BsHandIndex/> */}
+            <div className="card">
+                <div className="logo">
+
+                </div>
+                <div className="title"></div>
+                <div className="infoNav"></div>
+                <div className="paymentNav"></div>
+                <div className="outlet"></div>
+                <div className="pay"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
